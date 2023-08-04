8d29dc054137c99dca7caee6eaa2d7daa60d03d3
moifra3484 <moifra3484@ctag.com>
2023-08-04 09:03:03 +0200

    feat: remove item from car list after find car

--- test.py
+++ test.py
@@ -9,11 +9,12 @@ imagesRootPath = "C:/CIA/proba/"
 
 
 def searchImages(excelListSEMCar):
-    for dayFolder in os.listdir(imagesRootPath):
-        dayFolderPath = imagesRootPath + dayFolder
-        for carFolder in os.listdir(dayFolderPath):
-            carFolderPath = f"{dayFolderPath}/{carFolder}"
-            tempList = carFolder.split("_")
+    excelListSEMCar = list(excelListSEMCar)
+    for dayFolderName in os.listdir(imagesRootPath):
+        dayFolderPath = imagesRootPath + dayFolderName
+        for carFolderName in os.listdir(dayFolderPath):
+            carFolderPath = f"{dayFolderPath}/{carFolderName}"
+            tempList = carFolderName.split("_")
             semFolder = tempList[1]
 
             semFolderWithCero = semFolder[:5] + "0" + semFolder[5:]
@@ -21,14 +22,17 @@ def searchImages(excelListSEMCar):
             if (int(semFolderWithCero) in excelListSEMCar):
                 print(f"Encontrado SEM: {semFolder}")
                 # copiar carpeta e eliminar da lista excelListSEMCar
-                pathToCopy = f"{rootPath}/App_test/MivieCars/{carFolder}"
+                pathToCopy = f"{rootPath}/./MivieCars/{carFolderName}"
                 if not (os.path.exists(pathToCopy)):
                     shutil.copytree(f"{carFolderPath}",
                                     pathToCopy)
+                    # delete element array
+                    # no doy eliminado el elemento correspondiente
+                    excelListSEMCar.remove(int(semFolderWithCero))
+
                 else:
                     print("No copiado!Ya existe la carpeta!")
                     continue
-
             print(carFolderPath + "  SEM:" + tempList[1])
 
     for VIScar in excelListSEMCar:
